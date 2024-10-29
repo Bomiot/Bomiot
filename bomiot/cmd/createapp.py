ef8a258fc7abf4745a6186ec4879bbec4e2379d7
GreaterWMS <38828089+Singosgu@users.noreply.github.com>
2024-10-29 20:38:30 +0800

    V 0.3.5

--- bomiot/cmd/createapp.py
+++ bomiot/cmd/createapp.py
@@ -1,4 +1,3 @@
-import importlib
 from os.path import join, exists
 from os import makedirs, getcwd
 import os
@@ -24,23 +23,19 @@ def new_app(folder: str):
             current_path = Path(__file__).resolve()
             setup_path = join(getcwd(), 'setup.ini')
             app_configparser = ConfigParser()
-            app_configparser.read(join(setup_path, 'setup.ini'), encoding='utf-8')
-            project_name = app_configparser.get('project', 'name', fallback='bomiot')
+            app_configparser.read(setup_path, encoding='utf-8')
+            project_name = app_configparser.get('project', 'name')
             project_path = join(getcwd(), project_name)
-            bomiot_conf_check = importlib.import_module(f'{project_path}.bomiotconf')
-            if bomiot_conf_check.mode_return() == 'plugins':
-                print('Plugins can not create app')
+            app_path = join(project_path, sys.argv[2])
+            if exists(app_path):
+                print('App directory already exists')
             else:
-                app_path = join(project_path, sys.argv[2])
-                if exists(app_path):
-                    print('App directory already exists')
-                else:
-                    makedirs(app_path)
+                makedirs(app_path)
 
-                    copy_files(join(current_path.parent, 'extends'), app_path)
+                copy_files(join(current_path.parent, 'extends'), app_path)
 
-                    apps_path = join(app_path, 'apps.py')
-                    os.remove(apps_path)
-                    create_project_apps_py(apps_path, project_name, sys.argv[2])
+                apps_path = join(app_path, 'apps.py')
+                os.remove(apps_path)
+                create_project_apps_py(apps_path, project_name, sys.argv[2])
 
-                    print(f'Create APP success {sys.argv[2]}')
+                print(f'Create APP success {sys.argv[2]}')
