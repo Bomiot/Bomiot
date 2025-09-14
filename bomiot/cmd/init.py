729ef37677e5efe6652e4ca2d8ba7121ff0eaea1
GreaterWMS <38828089+Singosgu@users.noreply.github.com>
2025-09-14 14:55:50 +0800

    v1.1.3

--- bomiot/cmd/init.py
+++ bomiot/cmd/init.py
@@ -41,6 +41,8 @@ def create_file(folder: str):
     working_config = ConfigParser()
     working_path = join(join(current_path.parent.parent, 'server'), 'workspace.ini')
     working_config.read(working_path, encoding='utf-8')
+    if not working_config.has_section('space'):
+        working_config.add_section('space')
     working_config.set('space', 'name', working_space)
     working_config.write(open(working_path, "wt"))
 
